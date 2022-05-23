54aec870228e10ed6a21734025f4bceb6957f726
Mohammad Jaffal <mhmmdjaffal@gmail.com>
2022-05-23 10:06:23 +0300

    Creating random numbers

---
+++ JS/script-sheet.js
@@ -0,0 +1,15 @@
+window.onload = function (){
+
+
+var firstDice = Math.floor(Math.random() * 6) + 1;
+var secondDice = Math.floor(Math.random() * 6) + 1;
+
+console.log(firstDice)
+console.log(secondDice)
+
+
+
+
+
+
+}
