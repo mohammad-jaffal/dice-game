2309545c40a08ba8b056bcd917593664d9fa50a3
Mohammad Jaffal <mhmmdjaffal@gmail.com>
2022-05-23 10:17:25 +0300

    Adding algorithm

--- JS/script-sheet.js
+++ JS/script-sheet.js
@@ -1,15 +1,30 @@
-window.onload = function (){
+window.onload = function () {
+
+    var img1 = document.getElementsByClassName("img1")[0]
+    var img2 = document.getElementsByClassName("img2")[0]
+    var statusTitle = document.getElementById("status-title")
 
 
-var firstDice = Math.floor(Math.random() * 6) + 1;
-var secondDice = Math.floor(Math.random() * 6) + 1;
+    var firstDice = Math.floor(Math.random() * 6) + 1;
+    var secondDice = Math.floor(Math.random() * 6) + 1;
 
-console.log(firstDice)
-console.log(secondDice)
+    console.log(firstDice)
+    console.log(secondDice)
 
+    img1.src = `./assets/dice${firstDice}.png`
+    img2.src = `./assets/dice${secondDice}.png`
 
+    if (firstDice == secondDice){
+        statusTitle.textContent = "Draw!"
+    }
 
+    if (firstDice > secondDice){
+        statusTitle.textContent = "Player 1 wins! "
+    }
 
+    if (firstDice < secondDice){
+        statusTitle.textContent = "Player 2 wins! "
+    }
 
 
 }
