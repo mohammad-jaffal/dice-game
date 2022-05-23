bb2e4babbacba1c5f4c634fe4bf6a96c155a7d67
Mohammad Jaffal <mhmmdjaffal@gmail.com>
2022-05-23 10:59:57 +0300

    Adding on reload

--- JS/script-sheet.js
+++ JS/script-sheet.js
@@ -1,30 +1,35 @@
-window.onload = function () {
+// condition is true only if reload occured, no need for additional comments :)
+if (performance.navigation.type == performance.navigation.TYPE_RELOAD) {
+
 
     var img1 = document.getElementsByClassName("img1")[0]
     var img2 = document.getElementsByClassName("img2")[0]
     var statusTitle = document.getElementById("status-title")
+    var flagImg = document.getElementsByClassName("flag-img")[0]
 
 
     var firstDice = Math.floor(Math.random() * 6) + 1;
     var secondDice = Math.floor(Math.random() * 6) + 1;
 
-    console.log(firstDice)
-    console.log(secondDice)
 
     img1.src = `./assets/dice${firstDice}.png`
     img2.src = `./assets/dice${secondDice}.png`
 
-    if (firstDice == secondDice){
+    if (firstDice == secondDice) {
+        flagImg.style.display = "none"
         statusTitle.textContent = "Draw!"
     }
 
-    if (firstDice > secondDice){
-        statusTitle.textContent = "Player 1 wins! "
+    if (firstDice > secondDice) {
+        flagImg.style.display = "inline-block"
+        flagImg.src = "./assets/finish.png"
+        statusTitle.innerHTML = "Player 1 wins!"
     }
 
-    if (firstDice < secondDice){
-        statusTitle.textContent = "Player 2 wins! "
+    if (firstDice < secondDice) {
+        flagImg.style.display = "inline-block"
+        flagImg.src = "./assets/finish.png"
+        statusTitle.innerHTML = "Player 2 wins!"
     }
-
 
 }
